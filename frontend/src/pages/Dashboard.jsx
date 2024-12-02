50ea0bf2804e78a41919ab98268fa1f775e25c21
Minh Hieu Huynh <22520434@gm.uit.edu.vn>
2024-12-02 21:45:57 +0700

    ui: adjust position of dashboard card

--- frontend/src/pages/Dashboard.jsx
+++ frontend/src/pages/Dashboard.jsx
@@ -1,7 +1,5 @@
 // Dashboard.jsx
 import React from 'react';
-import Sidebar from '../components/Sidebar';
-import Header from '../components/Header';
 import DashboardCards from '../components/DashboardCards';
 import LineChart from '../components/LineChart';
 import ProductList from '../components/ProductList';
@@ -14,12 +12,14 @@ const Dashboard = () => {
   return (
     <div className="flex">
       <div className="flex-1 p-4 bg-gray-100">
-        <DashboardCards />
         <LineChart />
         <div className="grid grid-cols-2 gap-4">
           <ProductList title="Sản phẩm bán chạy" products={bestSelling} />
           <ProductList title="Sản phẩm sắp hết hạn" products={expiringProducts} />
         </div>
+      </div>
+      <div className="w-1/4 p-4">
+        <DashboardCards />
       </div>
     </div>
   );
